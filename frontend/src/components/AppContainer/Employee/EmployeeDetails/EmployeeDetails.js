e844339b981f068ab6699dadfd63017ac828403d
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-01-30 14:37:12 +0100

    make input fields editable

--- frontend/src/components/AppContainer/Employee/EmployeeDetails/EmployeeDetails.js
+++ frontend/src/components/AppContainer/Employee/EmployeeDetails/EmployeeDetails.js
@@ -3,6 +3,21 @@ import './EmployeeDetails.scss';
 import ProfileData from './ProfileData'
 
 class EmployeeDetails extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            readonly: true,
+        }
+    }
+
+    toggleEdit = (event) => {
+        this.setState({
+            readonly: !this.state.readonly,
+        })
+        console.log(this.state.readonly)
+    }
+
     render() {
         return(
             <div className='pages-container employee-details-wrapper'>
@@ -11,9 +26,14 @@ class EmployeeDetails extends React.Component {
                         <img src="" 
                             alt="" height='50px' width='50px'/>
                         <h1>Chad Johnson</h1>
+                        {this.state.readonly ? 
+                            <button className='profile-edit-btn' onClick={this.toggleEdit}>edit</button> :
+                            <button className='profile-save-btn' onClick={this.toggleEdit}>save</button> 
+                            }
+                        
                     </div>
                     
-                    <ProfileData />
+                    <ProfileData readonly={this.state.readonly}/>
 
                 </div>
             </div>
--- frontend/src/components/AppContainer/Employee/EmployeeDetails/ProfileData.js
+++ frontend/src/components/AppContainer/Employee/EmployeeDetails/ProfileData.js
@@ -3,39 +3,48 @@ import './EmployeeDetails.scss'
 
 class ProfileData extends React.Component {
     render() {
+        console.log(this.props.readonly)
         return(
             <div className='employee-profile-data-container'>
                 <div className='profile-table-element'>
                     <p>Firstname</p>
-                    <input type="text" value='Chad' readonly='true' />
+                    <input type="text" readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'}/>
                 </div>
                 <div className='profile-table-element'>
                     <p>Lastname</p>
-                    <input type="text" value='Johnson' readonly='true' />
+                    <input type="text" value='Johnson' readOnly={this.props.readonly}
+                    id={this.props.readonly ? '' : 'editable-input'}/>
                 </div>
                 <div className='profile-table-element'>
                     <p>AHV Number</p>
-                    <input type="text" value='13.45679.13' readonly='true' />
+                    <input type="text" value='13.45679.13' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
                 <div className='profile-table-element'>
                     <p>Phone</p>
-                    <input type="text" value='+41 79 523 98 45' readonly='true' />
+                    <input type="text" value='+41 79 523 98 45' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
                 <div className='profile-table-element'>
                     <p>Email</p>
-                    <input type="text" value='chad@johnson.com' readonly='true' />
+                    <input type="text" value='chad@johnson.com' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
                 <div className='profile-table-element'>
                     <p>Street</p>
-                    <input type="text" value='Minervastr. 25' readonly='true' />
+                    <input type="text" value='Minervastr. 25' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
                 <div className='profile-table-element'>
                     <p>Postal Code</p>
-                    <input type="text" value='8008' readonly='true' />
+                    <input type="text" value='8008' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
                 <div className='profile-table-element'>
                     <p>City</p>
-                    <input type="text" value='Zürich' readonly='true' />
+                    <input type="text" value='Zürich' readOnly={this.props.readonly} 
+                    id={this.props.readonly ? '' : 'editable-input'} />
                 </div>
             </div>
         )
