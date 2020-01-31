eea451b54b5fe3e42cd2b0f74fab75c4e1741f4e
sebscrm <40182748+sebscrm@users.noreply.github.com>
2020-01-31 14:30:14 +0100

    pdf.py fixes

--- backend/PDF/pdf.py
+++ backend/PDF/pdf.py
@@ -17,7 +17,6 @@ User = get_user_model()
 
 def payslip_pdf(request):
     payslip = get_object_or_404(User, pk=1)
-    # salary = get_object_or_404(User, pk=1)
     response = HttpResponse(content_type="application/pdf")
     response['Content-Disposition'] = "inline; filename={date}-{name}-payslip.pdf".format(
         date=payslip.date_created.strftime('%Y-%m-%d'),
@@ -26,7 +25,6 @@ def payslip_pdf(request):
     html = render_to_string(
         template_name="payslip_pdf.html", 
         context={'payslip': payslip,
-                 # 'salary': salary,
                     })
 
     host_url = request.META.get('wsgi.url_scheme') + '://' + request.META['HTTP_HOST']
@@ -37,32 +35,4 @@ def payslip_pdf(request):
     font_config = FontConfiguration()
     HTML(string=html).write_pdf(response, stylesheets=[css], font_config=font_config)
     return response
-
-
-# def letter_pdf(request, letter_id):
-#     letter = get_object_or_404(Letter, pk=letter_id)
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = (
-#         'inline; '
-#         f'filename={letter.created:%Y-%m-%d}-letter.pdf'
-#     )
-#     COMPONENTS = [
-#         'letters/pdf/cover.html',
-#         'letters/pdf/page01.html',
-#         'letters/pdf/page02.html',
-#         'letters/pdf/page03.html',
-#     ]
-#     documents = []
-#     font_config = FontConfiguration()
-#     for template_name in COMPONENTS:
-#         html = render_to_string(template_name, {
-#             'letter': letter,
-#         })
-#         document = HTML(string=html).render(font_config=font_config)
-#         documents.append(document)
-
-#     all_pages = [page for document in documents for page in document.pages]
-#     documents[0].copy(all_pages).write_pdf(response)
-
-#     return response
 
