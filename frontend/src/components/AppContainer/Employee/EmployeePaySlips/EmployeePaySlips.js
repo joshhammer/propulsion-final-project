374f25dcd9e08538c3f0939852dbabcbd268edfa
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-01-31 11:06:28 +0100

    set up component

---
+++ frontend/src/components/AppContainer/Employee/EmployeePaySlips/EmployeePaySlips.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EmployeePaySlips.scss'
+
+class EmployeePaySlips extends React.Component {
+    render() {
+        return(
+            <div>PaySlips</div>
+        )
+    }
+}
+
+export default EmployeePaySlips
