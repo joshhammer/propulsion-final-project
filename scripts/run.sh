887fcf8f1dd59a2bcb91d74dc93c7d56927dee19
sebscrm <40182748+sebscrm@users.noreply.github.com>
2020-01-31 11:53:24 +0100

    final changes on scripts before deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,7 @@ python manage.py collectstatic --no-input
 #/opt/conda/envs/app/bin/gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
 
-
+# to doucble check
 
 # -w stands for workers
 # -b stands for bind
