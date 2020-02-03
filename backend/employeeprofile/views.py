5ceb43b43156d5eccd23db2ad348ed6c4f6b4914
Rebecca Kurup Buchholz <rebeccakurup@protonmail.com>
2020-02-03 11:39:34 +0100

    Added corsheaders to settings.py and added IDs to admin string methods and attempted toggle deactivate employee

--- backend/employeeprofile/views.py
+++ backend/employeeprofile/views.py
@@ -1,7 +1,8 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView, get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from salary.permissions import IsAdmin
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -19,5 +20,50 @@ class GetMyEmployeeProfile(ListCreateAPIView):
         user = User.objects.filter(id=self.request.user.id)
         user.update(**request.data)
         return Response('Profile updated')
+
+
+class DeactivateEmployeeTest(GenericAPIView):
+    """
+    get:
+    toggle deactivate employee
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdmin]
+
+    lookup_field = 'user_id'
+
+
+
+
+class DeactivateEmployee(GenericAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdmin]
+
+    lookup_field = 'user_id'
+
+    def post(self, request, **kwargs):
+        #user = User.objects.filter(user_id=lookup_field)
+        #TODO
+        pass
+
+
+#  POST: Toggle activate an employee
+class ToggleDeactivateEmployee(GenericAPIView):
+
+    serializer_class = UserSerializer
+    permission_classes = [IsAdmin]
+    queryset = User.objects.all()
+
+    # the method which activates or deactivates an employee
+    def post(self, request, user_id):
+        employee = get_object_or_404(User, user_id=user_id)
+        if User in employee.is_active.all():
+            employee.is_active.remove(User)
+        else:
+            employee.is_active.add(User)
+        return Response(self.get_serializer(instance=employee).data)
+
 
 
