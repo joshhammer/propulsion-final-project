1313f4de539d4cadc5c9677550f7a0571183cdce
Rebecca Kurup Buchholz <rebeccakurup@protonmail.com>
2020-02-05 14:42:02 +0100

    Added extra info to employee serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from company.serializers import CompanySerializer
+from registration.serializers import RegistrationProfileTypeSerializer
 from salary.serializers import SalarySerializer
 
 User = get_user_model()
@@ -10,6 +12,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class UserProfileTypeSerializer(serializers.ModelSerializer):
+    registration = RegistrationProfileTypeSerializer()
+    company=CompanySerializer()
+    salary = SalarySerializer()
+
+    class Meta:
+        model = User
+        fields = ['salary', 'company', 'registration', 'id', 'email', 'first_name', 'last_name', 'bank_name', 'ahv', 'iban', 'house_number', 'street', 'city', 'postal_code', 'country']
 
 
 class UserLimitedSerializer(serializers.ModelSerializer):
