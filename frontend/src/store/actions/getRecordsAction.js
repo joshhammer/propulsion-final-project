e67fd926dc3841011948bfe25203032a20d82f37
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-02-07 14:59:16 +0100

    make payslips fetch real data

---
+++ frontend/src/store/actions/getRecordsAction.js
@@ -0,0 +1,19 @@
+import { GET_EMPLOYEE_RECORDS } from "../actionTypes"
+
+export const getRecordsAction = (token) => async (dispatch, getState) => {
+    const url = `https://razzpay.propulsion-learn.ch/api/employee/records/`
+    const headers = new Headers({
+        'Authorization': `Bearer ${token}`
+    })
+    const config = {
+        method: 'GET',
+        headers,
+    }
+    const response = await fetch(url, config)
+    const data = await response.json()
+    const action = {
+        type: GET_EMPLOYEE_RECORDS,
+        payload: data
+    }
+    dispatch(action)
+}
