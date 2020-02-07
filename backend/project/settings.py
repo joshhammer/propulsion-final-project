f70cb1d5f96afca0f5c7cb76af6fbd16705e00b1
Rebecca Kurup Buchholz <rebeccakurup@protonmail.com>
2020-02-07 16:17:28 +0100

    Changed the magical settings file in a frantic attempt at sanity

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -147,11 +147,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files/')
+MEDIA_ROOT = '/media-files/'
 STATIC_ROOT = '/static-files/'
 
 STATIC_URL = '/static-files/'
-MEDIA_URL = os.path.join(BASE_DIR, '/media-files/')
+MEDIA_URL = '/media-files/'
+#MEDIA_URL = os.path.join(BASE_DIR, '/media-files/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "user/static-files")
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 from record.pdf import payslip_pdf
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
-    #path('api/docs/', include_docs_urls(title='Django Template', schema_url='/', permission_classes=[])),
+    #path('api/docs/', include_docs_urls(title='Razzpay', public=False)),
     path('api/registration/', include('registration.urls')),
     path('api/auth/', include('registration.urls')),
     path('api/adminprofile/', include('adminprofile.urls')),
