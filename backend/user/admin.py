8f35e5cb0cf18dc8fef4ba73eb2ca613f0dd1e11
Rebecca Kurup Buchholz <rebeccakurup@protonmail.com>
2020-02-10 10:44:28 +0100

    Added company to List Records By Payment Date serializer

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -15,11 +15,11 @@ class UserAdmin(UserAdmin):
          ),
     )
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
+        (None, {'fields': ('email', 'username', 'password', 'company')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Groups', {'fields': ('groups',)}),
     )
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('email', 'company', 'first_name', 'last_name', 'is_staff')
     ordering = ('email',)
