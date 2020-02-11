b6bbed2384918fdf81cec17e368b59a6bc944902
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-02-11 16:45:06 +0100

    align styling of job and details pages

--- frontend/src/components/AppContainer/Employee/EmployeeJobandPay/EmployeeJobandPay.js
+++ frontend/src/components/AppContainer/Employee/EmployeeJobandPay/EmployeeJobandPay.js
@@ -20,17 +20,18 @@ class JobAndPay extends React.Component {
             <>
             {user && <div className='pages-container job-page-wrapper'>
                 <div className='employee-job-content'>
-                    <div className='employee-job-title-box'>
-                        <div className='job-title-and-logo'>
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="briefcase" className="svg-inline--fa fa-briefcase fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"></path></svg>
-                            <h1>Job & Pay</h1>
+                    <div className='job-pay-header'>
+                        <div className='employee-job-title-box'>
+                            <div className='job-title-and-logo'>
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="briefcase" className="svg-inline--fa fa-briefcase fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"></path></svg>
+                                <h1>Job & Pay</h1>
+                            </div>
                         </div>
                     </div>
                 <div className='employee-job-details-container'>
 
                         <h3 className='profile-data-section-title'>Basic Information</h3>
 
-                        <div className='job-details-box'>
                             <div className='job-table-element'>
                                 <p>Job Title</p>
                                 <input name="" type="text" value={user.salary.position}/>
@@ -43,11 +44,11 @@ class JobAndPay extends React.Component {
                                 <p>Salary</p>
                                 <input name="" type="text" value={user.salary.gross_month}/>
                             </div>
-                        </div>
+
 
                         <h3 className='profile-data-section-title'>Company Contact</h3>
 
-                        <div className='job-details-box'>
+                        
                             <div className='job-table-element'>
                                 <p>Full Name</p>
                                 <input name="" type="text" value={user.company.poc_name}/>
@@ -60,7 +61,7 @@ class JobAndPay extends React.Component {
                                 <p>Phone</p>
                                     <input name="" type="text" value={user.company.poc_phone}/>
                             </div>
-                        </div>
+                        
 
                         <h3 className='profile-data-section-title'>Company Address</h3>
 
