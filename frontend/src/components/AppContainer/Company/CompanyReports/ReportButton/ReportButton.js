11fe3473f684289e914de78deb6830b1b27b0cb8
amir_g <info@ghani.ch>
2020-02-11 10:59:47 +0100

    frontend - fixed icons etc.

--- frontend/src/components/AppContainer/Company/CompanyReports/ReportButton/ReportButton.js
+++ frontend/src/components/AppContainer/Company/CompanyReports/ReportButton/ReportButton.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './ReportButton.scss';
-import { ReactComponent as Info} from '../../../../../assets/svg/folder-plus.svg';
+import { ReactComponent as InfoPlus} from '../../../../../assets/svg/folder-plus.svg';
+import { ReactComponent as InfoMinus} from '../../../../../assets/svg/folder-minus.svg';
 
 const ReportButton = (props) => {
     return (
         <div className="toggle-button">
             <button onClick={props.toggle} className="report-button">
-                <Info width={25} height={25}/>
-                <p>{props.content}</p>
+                {
+                    props.hidden ? (<div className="button-div"><InfoPlus width={25} height={25}/><p>{props.content}</p></div>) : (<div className="button-div"><InfoMinus width={25} height={25}/><p>{props.content}</p></div>)
+                }
             </button>
         </div>
     )
