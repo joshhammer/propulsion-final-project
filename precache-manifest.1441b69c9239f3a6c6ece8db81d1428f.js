be53bfa921ebadd6e7231dfd5676bbea2819e83b
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-03-09 15:53:39 +0100

    Updates

---
+++ precache-manifest.1441b69c9239f3a6c6ece8db81d1428f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb06c9166c893d9266e8d60040821368",
+    "url": "/propulsion-final-project/index.html"
+  },
+  {
+    "revision": "9de6eb6dae4063a80cf5",
+    "url": "/propulsion-final-project/static/css/main.fb81f51e.chunk.css"
+  },
+  {
+    "revision": "4d2ccc634b4a69f7ed95",
+    "url": "/propulsion-final-project/static/js/2.b3c73fde.chunk.js"
+  },
+  {
+    "revision": "00541e5247cdd85cef72c468ed841769",
+    "url": "/propulsion-final-project/static/js/2.b3c73fde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9de6eb6dae4063a80cf5",
+    "url": "/propulsion-final-project/static/js/main.74e6e8eb.chunk.js"
+  },
+  {
+    "revision": "318df857e5b94cabaf56",
+    "url": "/propulsion-final-project/static/js/runtime-main.2ed3daa2.js"
+  },
+  {
+    "revision": "4afd377837f63c9de9ff1f164fe75b47",
+    "url": "/propulsion-final-project/static/media/facebook.4afd3778.svg"
+  },
+  {
+    "revision": "9667157d022a98baf0484c9592e116e0",
+    "url": "/propulsion-final-project/static/media/folder-minus.9667157d.svg"
+  },
+  {
+    "revision": "1c9bf5ce6732a02b6bf9bc919ad11a51",
+    "url": "/propulsion-final-project/static/media/folder-open.1c9bf5ce.svg"
+  },
+  {
+    "revision": "b187ed66baf64e3774fc09b01aac59cb",
+    "url": "/propulsion-final-project/static/media/folder-plus.b187ed66.svg"
+  },
+  {
+    "revision": "3302b02bf19d5a9c0f4572b2a12971b6",
+    "url": "/propulsion-final-project/static/media/instagram-square-brands.3302b02b.svg"
+  },
+  {
+    "revision": "128097fb7fe659f4b6732ff51fb8b245",
+    "url": "/propulsion-final-project/static/media/linkedin.128097fb.svg"
+  },
+  {
+    "revision": "c1f8074b2b55368671741d6bb33a926a",
+    "url": "/propulsion-final-project/static/media/success.c1f8074b.svg"
+  },
+  {
+    "revision": "fa8822b3287aff8c84a3b68ab6956b8f",
+    "url": "/propulsion-final-project/static/media/twitter.fa8822b3.svg"
+  },
+  {
+    "revision": "2d0f992488fc74592879212a98ef46fc",
+    "url": "/propulsion-final-project/static/media/user-plus.2d0f9924.svg"
+  }
+]);
